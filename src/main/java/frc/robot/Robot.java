f095f84284bf7eb3a525db6fece3aeac286bba4e
derpygamer2142 <150294967+derpysschoolaccount@users.noreply.github.com>
2025-01-18 09:24:42 -0600

    why no work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,10 +10,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with tank
@@ -30,10 +33,19 @@ public class Robot extends TimedRobot {
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
+    // SendableRegistry.addChild(m_robotDrive, m_leftMotor);
+    // SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    var lbc = new SparkMaxConfig();
+    // lbc.follow(1);
+    m_leftMotor.configure(lbc, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    var rbc = new SparkMaxConfig();
+    // rbc.follow(3);
+    m_rightMotor.configure(rbc, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
     SendableRegistry.addChild(m_robotDrive, m_leftMotor);
     SendableRegistry.addChild(m_robotDrive, m_rightMotor);
     SendableRegistry.addChild(m_robotDrive, m_leftBackMotor);
     SendableRegistry.addChild(m_robotDrive, m_rightBackMotor);
+    
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
@@ -46,6 +58,7 @@ public class Robot extends TimedRobot {
     // That means that the Y axis of the left stick moves the left side
     // of the robot forward and backward, and the Y axis of the right stick
     // moves the right side of the robot forward and backward.
+    System.out.println("9jadwuih9huj98fe89f");
     m_robotDrive.tankDrive(-m_driverController.getLeftY(), -m_driverController.getRightY());
   }
 }
