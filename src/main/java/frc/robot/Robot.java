1624b862413ef12c800dbb009aab02ea55f6a046
derpysschoolaccount <150294967+derpysschoolaccount@users.noreply.github.com>
2025-02-12 21:49:37 -0600

    possible fixes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 // import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import com.revrobotics.spark.SparkLowLevel;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -28,9 +29,9 @@ import frc.robot.external.LIDARLite;
  */
 public class Robot extends TimedRobot {
   private final SparkMax m_leftMotor = new SparkMax(1, SparkLowLevel.MotorType.kBrushed);
-  private final TalonFX m_rightMotor = new TalonFX(3);
+  private final WPI_TalonSRX m_rightMotor = new WPI_TalonSRX(3);
   private final SparkMax m_leftBackMotor = new SparkMax(2, SparkLowLevel.MotorType.kBrushed);
-  private final TalonFX m_rightBackMotor = new TalonFX(4); // note: as of 2/8/25 we still need to flash these
+  private final WPI_TalonSRX m_rightBackMotor = new WPI_TalonSRX(4); // note: as of 2/8/25 we still need to flash these
   private final SparkMax m_coralLoader = new SparkMax(6, SparkLowLevel.MotorType.kBrushed);
   private final DifferentialDrive m_robotDrive =
       new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
@@ -46,10 +47,7 @@ public class Robot extends TimedRobot {
     var lbc = new SparkMaxConfig();
     //lbc.follow(1);
     m_leftMotor.configure(lbc, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    var rbc = new TalonFXConfiguration();
-    //rbc.follow(3);
-    rbc.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-    m_rightMotor.getConfigurator().apply(rbc);
+    m_rightMotor.setInverted(true);
     SendableRegistry.addChild(m_robotDrive, m_leftMotor);
     SendableRegistry.addChild(m_robotDrive, m_rightMotor);
     SendableRegistry.addChild(m_robotDrive, m_leftBackMotor);
